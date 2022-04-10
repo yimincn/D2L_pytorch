2e06a3dd6cc25ecd45cf29165e8bc744ae7af50b
yimincn <yiminzh@outlook.com>
2022-04-11 00:19:37 +0800

    update chapter_18_appendix-mathematics-for-deep-learning_eigendecomposition.py

---
+++ chapter_18_appendix-mathematics-for-deep-learning_eigendecomposition.py
@@ -0,0 +1,70 @@
+# %matplotlib inline
+import torch
+from IPython import display
+from d2l import torch as d2l
+
+torch.eig(torch.tensor([[2, 1], [2, 3]], dtype=torch.float64), eigenvectors=True)
+
+A = torch.tensor([[1.0, 0.1, 0.1, 0.1],
+              [0.1, 3.0, 0.2, 0.3],
+              [0.1, 0.2, 5.0, 0.5],
+              [0.1, 0.3, 0.5, 9.0]])
+
+v, _ = torch.eig(A)
+print(v)
+
+torch.manual_seed(42)
+
+k = 5
+A = torch.randn(k, k, dtype=torch.float64)
+print(A)
+
+
+# Calculate the sequence of norms after repeatedly applying `A`
+v_in = torch.randn(k, 1, dtype=torch.float64)
+
+norm_list = [torch.norm(v_in).item()]
+for i in range(1, 100):
+    v_in = A @ v_in
+    norm_list.append(torch.norm(v_in).item())
+
+d2l.plot(torch.arange(0, 100), norm_list, 'Iteration', 'Value')
+
+
+# Compute the scaling factor of the norms
+norm_ratio_list = []
+for i in range(1, 100):
+    norm_ratio_list.append(norm_list[i]/norm_list[i - 1])
+
+d2l.plot(torch.arange(1, 100), norm_ratio_list, 'Iteration', 'Ratio')
+
+
+# Compute the eigenvalues
+eigs = torch.eig(A)[0][:,0].tolist()
+norm_eigs = [torch.abs(torch.tensor(x)) for x in eigs]
+norm_eigs.sort()
+print(f'norms of eigenvalues: {norm_eigs}')
+
+
+# Rescale the matrix `A`
+A /= norm_eigs[-1]
+
+# Do the same experiment again
+v_in = torch.randn(k, 1, dtype=torch.float64)
+
+norm_list = [torch.norm(v_in).item()]
+for i in range(1, 100):
+    v_in = A @ v_in
+    norm_list.append(torch.norm(v_in).item())
+
+d2l.plot(torch.arange(0, 100), norm_list, 'Iteration', 'Value')
+
+# Also plot the ratio
+norm_ratio_list = []
+for i in range(1, 100):
+    norm_ratio_list.append(norm_list[i]/norm_list[i-1])
+
+d2l.plot(torch.arange(1, 100), norm_ratio_list, 'Iteration', 'Ratio')
+
+
+
