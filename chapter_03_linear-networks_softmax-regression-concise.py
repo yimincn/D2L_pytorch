622805fba7df1e789c927dfea202d65e71efccc2
yimincn <yiminzh@outlook.com>
2022-04-16 23:40:25 +0800

    update chapter_04

--- chapter_03_linear-networks_softmax-regression-concise.py
+++ chapter_03_linear-networks_softmax-regression-concise.py
@@ -10,19 +10,18 @@ train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 # layer to reshape the inputs before the linear layer in our network
 net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
 
+
 def init_weights(m):
     if type(m) == nn.Linear:
         nn.init.normal_(m.weight, std=0.01)
 
-net.apply(init_weights);
 
+net.apply(init_weights)
 
 loss = nn.CrossEntropyLoss(reduction='none')
 
-
 trainer = torch.optim.SGD(net.parameters(), lr=0.1)
 
-
-num_epochs = 10
+num_epochs = 20
 d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
-
+d2l.plt.show()
--- chapter_03_linear-networks_softmax-regression-scratch.py
+++ chapter_03_linear-networks_softmax-regression-scratch.py
@@ -12,9 +12,10 @@ num_outputs = 10
 W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
 b = torch.zeros(num_outputs, requires_grad=True)
 
-
 X = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-X.sum(0, keepdim=True), X.sum(1, keepdim=True)
+print(X.sum(0, keepdim=True))
+print(X.sum(0))
+print(X.sum(1, keepdim=True))
 
 
 def softmax(X):
@@ -25,7 +26,8 @@ def softmax(X):
 
 X = torch.normal(0, 1, (2, 5))
 X_prob = softmax(X)
-X_prob, X_prob.sum(1)
+print(X_prob)
+print(X_prob.sum(1))
 
 
 def net(X):
@@ -34,18 +36,18 @@ def net(X):
 
 y = torch.tensor([0, 2])
 y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
-y_hat[[0, 1], y]
-
+print(y_hat[[0, 1], y])
 
 
 def cross_entropy(y_hat, y):
     return - torch.log(y_hat[range(len(y_hat)), y])
 
-cross_entropy(y_hat, y)
+
+print(cross_entropy(y_hat, y))
+print("="*100)
 
 
-
-def accuracy(y_hat, y):  #@save
+def accuracy(y_hat, y):  # @save
     """Compute the number of correct predictions."""
     if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
         y_hat = y_hat.argmax(axis=1)
@@ -53,9 +55,10 @@ def accuracy(y_hat, y):  #@save
     return float(cmp.type(y.dtype).sum())
 
 
-accuracy(y_hat, y) / len(y)
+print(accuracy(y_hat, y) / len(y))
 
-def evaluate_accuracy(net, data_iter):  #@save
+
+def evaluate_accuracy(net, data_iter):  # @save
     """Compute the accuracy for a model on a dataset."""
     if isinstance(net, torch.nn.Module):
         net.eval()  # Set the model to evaluation mode
@@ -67,8 +70,9 @@ def evaluate_accuracy(net, data_iter):  #@save
     return metric[0] / metric[1]
 
 
-class Accumulator:  #@save
+class Accumulator:  # @save
     """For accumulating sums over `n` variables."""
+
     def __init__(self, n):
         self.data = [0.0] * n
 
@@ -81,9 +85,11 @@ class Accumulator:  #@save
     def __getitem__(self, idx):
         return self.data[idx]
 
-evaluate_accuracy(net, test_iter)
 
-def train_epoch_ch3(net, train_iter, loss, updater):  #@save
+print(evaluate_accuracy(net, test_iter))
+
+
+def train_epoch_ch3(net, train_iter, loss, updater):  # @save
     """The training loop defined in Chapter 3."""
     # Set the model to training mode
     if isinstance(net, torch.nn.Module):
@@ -107,12 +113,12 @@ def train_epoch_ch3(net, train_iter, loss, updater):  #@save
     # Return training loss and training accuracy
     return metric[0] / metric[2], metric[1] / metric[2]
 
-class Animator:  #@save
+
+class Animator:  # @save
     """For plotting data in animation."""
-    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
-                 ylim=None, xscale='linear', yscale='linear',
-                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
-                 figsize=(3.5, 2.5)):
+
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None, ylim=None, xscale='linear',
+                 yscale='linear', fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1, figsize=(3.5, 2.5)):
         # Incrementally plot multiple lines
         if legend is None:
             legend = []
@@ -148,39 +154,40 @@ class Animator:  #@save
         display.clear_output(wait=True)
 
 
-
-
-def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):  #@save
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):  # @save
     """Train a model (defined in Chapter 3)."""
-    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
-                        legend=['train loss', 'train acc', 'test acc'])
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9], legend=['train loss', 'train acc', 'test acc'])
     for epoch in range(num_epochs):
         train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
         test_acc = evaluate_accuracy(net, test_iter)
         animator.add(epoch + 1, train_metrics + (test_acc,))
     train_loss, train_acc = train_metrics
     assert train_loss < 0.5, train_loss
-    assert train_acc <= 1 and train_acc > 0.7, train_acc
-    assert test_acc <= 1 and test_acc > 0.7, test_acc
+    assert 1 >= train_acc > 0.7, train_acc
+    assert 1 >= test_acc > 0.7, test_acc
 
 
 lr = 0.1
 
+
 def updater(batch_size):
     return d2l.sgd([W, b], lr, batch_size)
+
 
 num_epochs = 10
 train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
 
 
-def predict_ch3(net, test_iter, n=6):  #@save
+def predict_ch3(net, test_iter, n=6):  # @save
     """Predict labels (defined in Chapter 3)."""
     for X, y in test_iter:
         break
     trues = d2l.get_fashion_mnist_labels(y)
     preds = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1))
-    titles = [true +'\n' + pred for true, pred in zip(trues, preds)]
-    d2l.show_images(
-        X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
+    titles = [true + '\n' + pred for true, pred in zip(trues, preds)]
+    d2l.show_images(X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
+
 
 predict_ch3(net, test_iter)
+d2l.plt.show()
+
